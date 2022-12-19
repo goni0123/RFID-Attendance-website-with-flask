40631a8b70c2d1e4dabf55e81c80158d0fbbd9b4
Agon Muharemi <am29205@seeu.edu.mk>
2022-12-19 23:27:01 +0100

    success the registration

--- app.py
+++ app.py
@@ -1,14 +1,8 @@
 import RPi.GPIO as GPIO
 from flask import Flask, render_template, request
 from mfrc522 import SimpleMFRC522
-from pymysql
+import mariadb
 reader = SimpleMFRC522()
-
-def sql_connector():
-  conn=pymysql.conncect(user='admin',password='password',db='Attended',host='localhost')
-  c=conn.crusor()
-  return conn,c
-
 
 app =Flask(__name__)
 
@@ -20,32 +14,35 @@ def Attendance():
 
 @app.route('/Adduser', methods=['GET','POST'] )
 def Adduser():
-  rfid, text = reader.read()
-  while rfid < 0:
+  if request.method=='POST':
     rfid, text = reader.read()
-  id=request.form.get('id')
-  name=request.form.get('name')
-
-  conn, c = sql_conncetor()
-  c.execute("INSERT INTO users VALUES ({},'{}',{})".format(int(id),name,int(rfid))
-  conn.commit()
-  conn.close()
-  c.close()
+    while rfid < 0:
+      rfid, text = reader.read()
+    id=request.form.get('id')
+    name=request.form.get('name')
+    conn = mariadb.connect(user='admin', password='password',db='Attended', host='localhost')
+    c = conn.cursor()
+    c.execute("INSERT INTO users (id,rfid_uid,name) VALUES ({},'{}',{})".format(int(id), name, int(rfid)))
+    conn.commit()
+    conn.close()
+    c.close()
   return render_template('Adduser.html') 
 
-@app.route('/Addevent', methods=['GET'],['POST'])
+@app.route('/Addevent', methods=['GET','POST'])
 def Addevent():
-  Event_id=request.form.get('Event_id')
-  Event_name=request.form.get('Event_name')
-  Data_start=request.form.get('Data_start')
-  Data_finish=request.form.get('Data_finish')
-  conn, c =sql_conncetor()
-  c.execute("INSERT INTO Event VALUES ({},'{}',{},{})".format(int(Event_id), Event_name, Data_start, Data_finish)
-  conn.commit()
-  conn.close()
-  c.close()
+  if request.method=='POST':
+    Event_id=request.form.get('Event_id')
+    Event_name=request.form.get('Event_name')
+    Data_start=request.form.get('Data_start')
+    Data_finish=request.form.get('Data_finish')
+    conn=mariadb.connect(user='admin', password='password',db='Attended', host='localhost')
+    c=conn.cursor()
+    c.execute("INSERT INTO Event(Event_id,Event_name,Data_start,Data_finish) VALUES ({},'{}','{}','{}')".format(int(Event_id), Event_name, Data_start, Data_finish))
+    conn.commit()
+    conn.close()
+    c.close()
   return render_template('Addevent.html')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
